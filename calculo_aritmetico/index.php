ecc7090ba320b549218a75dae2edfb328cc7aa55
Sandro Da Silva Nunes <04876125155@DCCBRA.IFB.LOCAL>
2024-03-14 21:00:01 -0300

    Calculo Aritmético

---
+++ calculo_aritmetico/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculo Aritmético</title>
+</head>
+<body>
+    <h1>CALCULO ARITMÉTICO</h1>
+        <form action="resultado.php" method = post>
+        <p>
+            Nome do aluno:
+            <input type="text" name = "nome"> <br><br>
+
+            Nota 1 :
+            <input type="text" name = "num1"> <br> <br>
+            Nota 2 :
+            <input type="text" name = "num2"> <br> <br>
+            Nota 3 :
+            <input type="text" name = "num3"> <br> <br>
+            Nota 4 :
+            <input type="text" name = "num4"> <br> <br>
+           
+            
+            <input type="Submit" value = "FODA - SE"> <br> <br>
+
+        </p>
+
+
+
+        </form>
+    
+</body>
+</html>
---
+++ calculo_aritmetico/resultado.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculo Aritmético</title>
+</head>
+<body>
+    <h1>RESULTADO FINAL</h1>
+
+<?php
+
+    $nome = $_POST["nome"];
+
+    $num1 = $_POST["num1"];
+    $num2 = $_POST["num2"];
+    $num3 = $_POST["num3"];
+    $num4 = $_POST["num4"];
+
+    $soma = ($num1 + $num2 + $num3 + $num4)/4;
+
+    echo"<p>Aluno: $nome </p>";
+    echo "<p>Nota final $soma </p>";
+
+
+
+
+
+
+
+?>
+    
+</body>
+</html>
